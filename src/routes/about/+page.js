aee16a2550dd92ac0f1eb971a29fd1a1ed84f75d
백주연 <juurom@baegjuyeon-ui-MacBookAir.local>
2024-02-21 05:46:53 +0900

    fix: fetch 주소 변경

--- src/routes/about/+page.js
+++ src/routes/about/+page.js
@@ -1,5 +1,5 @@
 export async function load({fetch, request}){
-    let res = await fetch('http://localhost:5173/api')
+    let res = await fetch('/api')
     let result = await res.json()
     //console.log(result)
     return result;
